782a045bcffc9734a4fb3f3e060d52c38f876eef
Anthony SERRANO <34anthonyserrano@gmail.com>
2024-04-25 14:54:23 +0200

    :memo: Ajout de critiques sur TennisGame1.js

--- javascript/TennisGame1.js
+++ javascript/TennisGame1.js
@@ -1,4 +1,19 @@
+// Critiques :
+// - Les noms de variables comme m_score1, m_score2, tempScore et minusResult ne sont pas très "explicites" et peuvent être difficile à comprendre pour quelqu'un qui lit le code pour la première fois.
+// - Tout dépend des préférences personnelles mais il serait intéressant d'ajouter des commentaires avant chaque fonction pour expliquer brièvement ce qu'elle fait afin de comprendre rapidement la logique derrière le code pour assurer une meilleure maintenabilité.
+// - Il serait aussi intéressant d'ajouter des commentaires avant les fonctions pour documenter les paramètres attendus et les valeurs de retour.
+// - L'indentation du code n'est pas uniforme ce qui rend la lecture du code difficile.
+// - Plusieurs structures de contrôle imbriquées (if-else, switch, boucle for), ce qui rend là aussi la logique du code difficile à suivre.
+// - Le code ne gère pas tous les cas possibles, par exemple, il ne traite pas le cas où les scores dépassent 3 sans être égaux.
+// - Bien que l'utilisation des prototypes en JavaScript soit courante pour ajouter des méthodes à un objet, cela peut parfois rendre le code moins intuitif.
+// - Le code semble être une seule grande fonction, ce qui peut rendre difficile la réutilisation de parties du code dans d'autres contextes ou la mise à jour de fonctionnalités spécifiques.
+// - Le code vérifie si window est défini pour déterminer s'il s'exécute dans un navigateur ou dans un environnement Node.js, ce qui peut rendre le code moins portable et moins modulaire.
+// - Le code n'utilise pas de syntaxe JavaScript moderne comme par exemple `let` et `const` au lieu de `var` pour la déclaration des variables.
+// - Le code ne gère pas les erreurs.
+// - La fonction getScore pourrait être beaucoup plus simplifiée.
+
 var TennisGame1 = function(player1Name, player2Name) {
+    
     this.m_score1 = 0;
     this.m_score2 = 0;
     this.player1Name = player1Name;
